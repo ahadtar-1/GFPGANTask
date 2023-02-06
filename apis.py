2e600eaec4afe8306cc009513c99914d64a43d69
ahadtar-1 <93494109+ahadtar-1@users.noreply.github.com>
2023-02-07 01:23:58 +0500

    Add files via upload

--- apis.py
+++ apis.py
@@ -4,7 +4,7 @@ The module comprises of the different APIs
 
 from flask import Flask, request, send_file
 import os
-from operations import upsampling_image
+from operations import upsampling_image, face_enhancement
 
 app = Flask(__name__)
 
@@ -29,6 +29,32 @@ def upsample_img()-> str:
         fileName = f.filename
         f.save(os.path.join("",fileName))
         path = upsampling_image(os.path.join("",fileName))
+        if path:
+            return send_file(path)
+        else:
+            return "No image"
+
+
+@app.route('/enhanceimg', methods = ['GET', 'POST'])
+def enhance_img()-> str:
+    """
+    The api function that generates an enhanced image from a noisy image
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    str
+    
+    """
+
+    if request.method == 'POST':
+        f = request.files['file']
+        fileName = f.filename
+        f.save(os.path.join("",fileName))
+        path = face_enhancement(os.path.join("",fileName))
         if path:
             return send_file(path)
         else:
